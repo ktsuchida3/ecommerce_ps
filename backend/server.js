718df2709ae650fdec8185fa318b05d1a84b2967
tochidz <ktsuchida3@gmail.com>
2023-09-03 00:22:46 -0700

    JWT HTTP cookie implemented

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -12,11 +12,16 @@ connectDB(); //connect to MongoDB
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
