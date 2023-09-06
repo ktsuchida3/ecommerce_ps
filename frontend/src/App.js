9e22da71bc4815fdf80ccefb68e442ccf570700e
tochidz <ktsuchida3@gmail.com>
2023-09-06 01:05:55 -0700

    user login/logout complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -13,8 +16,9 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
 
-export default App
+export default App;
