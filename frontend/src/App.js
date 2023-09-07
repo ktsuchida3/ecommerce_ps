e16d4403945221c4a1b7e9778b6a732012272146
tochidz <ktsuchida3@gmail.com>
2023-09-07 01:12:59 -0700

    shipping screen and order routes completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import { Container } from "react-bootstrap"
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main className='py-3'>
         <Container>
@@ -16,7 +19,6 @@ const App = () => {
         </Container>
       </main>
       <Footer />
-      <ToastContainer />
     </>
   );
 };
