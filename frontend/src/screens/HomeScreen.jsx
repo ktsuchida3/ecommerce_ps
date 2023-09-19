53dd6eb6f2a284b6572aab7eaa2796f770cbc490
tochidz <ktsuchida3@gmail.com>
2023-09-19 15:37:56 -0700

    product search implemented

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -7,12 +7,17 @@ import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
   return (
     <>
+      { keyword && (
+        <Link to='/' className='btn btn-light mb-4'>
+          Go Back
+        </Link>
+      )}
       { isLoading ? (
           <Loader />
       ) : error ? (
@@ -31,7 +36,8 @@ const HomeScreen = () => {
           </Row>
           <Paginate
             pages={data.pages}
-            page={data.page}/>
+            page={data.page}
+            keyword={keyword ? keyword : ''}/>
         </>
       )}
     </>
